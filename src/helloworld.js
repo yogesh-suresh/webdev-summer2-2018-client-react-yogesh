457c67470c15553d07c20ca89f270eab7303c54d
Yogesh Suresh <suresh.y@husky.neu.edu>
2018-07-16 12:55:02 -0400

    hello world

---
+++ src/helloworld.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Helloworld extends React.Component {
+    render(){
+        return (
+            <h1>Hello World efew</h1>
+        )
+    }
+}
+
+export default Helloworld;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Helloworld from './helloworld'
+
+ReactDOM.render(
+  <Helloworld/>,
+  document.getElementById('root')
+
+);
+
+
